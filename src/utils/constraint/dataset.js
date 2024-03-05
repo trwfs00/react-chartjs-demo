1b94c691603a3697e6005040f161dcbd40672816
trwfs00 <teerawut.s@kkumail.com>
2024-03-05 14:20:58 +0700

    - fix: confilct on merge dataset

--- src/utils/constraint/dataset.js
+++ src/utils/constraint/dataset.js
@@ -14,115 +14,6 @@ const randomFixedColor = () => {
 }
 
 const labels = [
-    "0",
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "11",
-    "12",
-    "13",
-    "14",
-    "15",
-    "16",
-    "17",
-    "18",
-    "19",
-    "20",
-    "21",
-    "22",
-    "23",
-    "24",
-    "25",
-    "26",
-    "27",
-    "28",
-    "29",
-    "30",
-  ]
-
-  const company = []
-  const bid = []
-
-  const dataset = [
-    {
-      companyname:'1 เฮลท์แคร์ จำกัด',
-      bid:1500000
-    },
-    {
-      companyname:'1 บวก 2 พี่น้องกัน จำกัด',
-      bid:1550000
-    },
-    {
-      companyname:'Praut Company',
-      bid:1600000
-    },
-    {
-      companyname:'1 เฮลท์แคร์ จำกัด',
-      bid:1650000
-    },
-    {
-      companyname:'1 เฮลท์แคร์ จำกัด',
-      bid:1700000
-    },
-    {
-      companyname:'1 บวก 2 พี่น้องกัน จำกัด',
-      bid:1701000
-    },
-    {
-      companyname:'1 เฮลท์แคร์ จำกัด',
-      bid:1750000
-    },
-    {
-      companyname:'Praut Company',
-      bid:1800000
-    },
-    {
-      companyname:'1 บวก 2 พี่น้องกัน จำกัด',
-      bid:1850000
-    },
-    {
-      companyname:'1 เฮลท์แคร์ จำกัด',
-      bid:1900000
-    },
-  ]
-
-
-
-  dataset.forEach(item => {
-    // company.push(item.companyname);
-    bid.push(item.bid);
-  });
-
-  console.log(company)
-  console.log(bid)
-
-export const data_english = {
-    labels,
-    datasets: [
-      {
-        label: '1 บวก 2 พี่น้องกัน จำกัด',
-        data: bid,
-        // borderColor: ["rgb(11, 78, 128)","rgb(255, 255, 132)"],
-        backgroundColor: ["rgb(11, 78, 128)","rgb(255, 255, 132)"],
-        borderColor: "rgb(0,0,0)",
-        pointRadius: 5,
-        borderWidth: 1.2,
-        drawBorder: false,
-        pointBorderColor: ["rgb(11, 78, 128)","rgb(255, 255, 132)"],
-      }
-    ],
-  }
-  
-  export const data_sealed = {
-    labels,
-    datasets: [
   "0",
   "1",
   "2",
@@ -155,6 +46,77 @@ export const data_english = {
   "29",
   "30",
 ]
+
+const company = []
+const bid = []
+
+const dataset = [
+  {
+    companyname: "1 เฮลท์แคร์ จำกัด",
+    bid: 1500000,
+  },
+  {
+    companyname: "1 บวก 2 พี่น้องกัน จำกัด",
+    bid: 1550000,
+  },
+  {
+    companyname: "Praut Company",
+    bid: 1600000,
+  },
+  {
+    companyname: "1 เฮลท์แคร์ จำกัด",
+    bid: 1650000,
+  },
+  {
+    companyname: "1 เฮลท์แคร์ จำกัด",
+    bid: 1700000,
+  },
+  {
+    companyname: "1 บวก 2 พี่น้องกัน จำกัด",
+    bid: 1701000,
+  },
+  {
+    companyname: "1 เฮลท์แคร์ จำกัด",
+    bid: 1750000,
+  },
+  {
+    companyname: "Praut Company",
+    bid: 1800000,
+  },
+  {
+    companyname: "1 บวก 2 พี่น้องกัน จำกัด",
+    bid: 1850000,
+  },
+  {
+    companyname: "1 เฮลท์แคร์ จำกัด",
+    bid: 1900000,
+  },
+]
+
+dataset.forEach(item => {
+  // company.push(item.companyname);
+  bid.push(item.bid)
+})
+
+console.log(company)
+console.log(bid)
+
+export const data_english = {
+  labels,
+  datasets: [
+    {
+      label: "1 บวก 2 พี่น้องกัน จำกัด",
+      data: bid,
+      // borderColor: ["rgb(11, 78, 128)","rgb(255, 255, 132)"],
+      backgroundColor: ["rgb(11, 78, 128)", "rgb(255, 255, 132)"],
+      borderColor: "rgb(0,0,0)",
+      pointRadius: 5,
+      borderWidth: 1.2,
+      drawBorder: false,
+      pointBorderColor: ["rgb(11, 78, 128)", "rgb(255, 255, 132)"],
+    },
+  ],
+}
 
 export const reverse_auto_generate = {
   labels: labels.slice(0, 20),
