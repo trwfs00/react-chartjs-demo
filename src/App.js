f5b698de6ab03a2cf0c308aa1e904998a2b25d04
Jxhnnny <112389734+NatpacanSri@users.noreply.github.com>
2024-03-06 23:38:11 +0700

    feat: func compute data sealed

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ import ChartTest from "./components/test"
 import CustomLegendChart from "./components/test"
 
 const english_revert = require('./utils/english_revert.json')
+const sealed_forward = require('./utils/sealed_forward.json')
 
 ChartJS.register(
   CategoryScale,
@@ -36,34 +37,34 @@ const labels = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
 
 // const chartData = {
 //   labels: labels,
-//   datasets: [
-//     {
-//       label: "โง่",
-//       data: [null, 1500000, null],
-//       borderColor: "#000", // Customize line color
-//       fill: false, // Do not fill area under the line
-//       pointBackgroundColor: "#043daf",
-//       pointBorderColor: "#fff",
-//       pointRadius: 7,
-//     },
-//     {
-//       label: "ฉลาด",
-//       data: [null, null, 1510000],
-//       borderColor: "#000", // Customize line color
-//       fill: false, // Do not fill area under the line
-//       pointBackgroundColor: "#52ad21",
-//       pointBorderColor: "#fff",
-//       pointRadius: 7,
-//     },
-//     {
-//       label: "ไม่ฉลาด",
-//       data: [null, null, null, 1505000],
-//       borderColor: "#000", // Customize line color
-//       fill: false, // Do not fill area under the line
-//       pointBackgroundColor: "#9123ad",
-//       pointBorderColor: "#fff",
-//       pointRadius: 7,
-//     },
+// datasets: [
+//   {
+//     label: "โง่",
+//     data: [null, 1500000, null],
+//     borderColor: "#000", // Customize line color
+//     fill: false, // Do not fill area under the line
+//     pointBackgroundColor: "#043daf",
+//     pointBorderColor: "#fff",
+//     pointRadius: 7,
+//   },
+//   {
+//     label: "ฉลาด",
+//     data: [null, null, 1510000],
+//     borderColor: "#000", // Customize line color
+//     fill: false, // Do not fill area under the line
+//     pointBackgroundColor: "#52ad21",
+//     pointBorderColor: "#fff",
+//     pointRadius: 7,
+//   },
+//   {
+//     label: "ไม่ฉลาด",
+//     data: [null, null, null, 1505000],
+//     borderColor: "#000", // Customize line color
+//     fill: false, // Do not fill area under the line
+//     pointBackgroundColor: "#9123ad",
+//     pointBorderColor: "#fff",
+//     pointRadius: 7,
+//   },
 //     {
 //       data: [null, 1500000, 1510000, 1505000],
 //       borderColor: "#000", // Customize line color
@@ -96,7 +97,7 @@ const computeDataEnglish = (data) => {
   data.result.forEach(result => {
     const companyName = result.detail.company_name;
     const biddingAmount = result.detail.bidding_amount;
-    const biddingTime = new Date(result.detail.bidding_time).toLocaleDateString('en-US'); // Format bidding time to MM-DD-YYYY
+    const biddingTime = new Date(result.detail.bidding_time) // Format bidding time to MM-DD-YYYY
 
     if (!companies[companyName]) {
       companies[companyName] = {
@@ -105,13 +106,13 @@ const computeDataEnglish = (data) => {
       }
     }
 
-    const index  = data.result.length - data.result.indexOf(result) - 1; // Calculate the index from the end
+    const index = data.result.length - data.result.indexOf(result) - 1; // Calculate the index from the end
     // companies[companyName][index] = biddingAmount; // Insert the bidding amount at the calculated index
     companies[companyName].biddingAmounts[index] = biddingAmount; // Insert the bidding amount at the calculated index
     companies[companyName].biddingTimes[index] = biddingTime; // Insert the bidding time at the calculated index
   });
 
-  const transformedData = Object.entries(companies).map(([companyName, {biddingAmounts, biddingTimes}],index) => {
+  const transformedData = Object.entries(companies).map(([companyName, { biddingAmounts, biddingTimes }], index) => {
     biddingAmounts.unshift(null); // Add null at the beginning of the array
     biddingTimes.unshift(null); // Add null at the beginning of the array
     return {
@@ -126,12 +127,10 @@ const computeDataEnglish = (data) => {
     }
   });
 
-  console.log('transformedData:', transformedData)
-
   // จัด data ให้เส้น
   const sortedBiddingAmounts = data.result.map(item => item.detail.bidding_amount).sort((a, b) => b - a);
   sortedBiddingAmounts.unshift(null);
-  
+
   const chartEngData = {
     labels: labels,
     datasets: [
@@ -147,7 +146,47 @@ const computeDataEnglish = (data) => {
       },
     ]
   }
+  console.log("english:", chartEngData)
   return chartEngData
+}
+
+const computeDataSealed = (data) => {
+  const result = [];
+  const companyData = {};
+
+  data.result.forEach((item) => {
+    const { company_name, bidding_amount, bidding_time } = item.detail;
+    if (!companyData[company_name]) {
+      companyData[company_name] = [];
+    }
+    companyData[company_name].push({ bidding_amount, bidding_time });
+  });
+
+  console.log('companyData:', companyData)
+
+  Object.entries(companyData).forEach(([key, value], index) => {
+    let bids = value.map(item => item.bidding_amount);
+    // let bidDate = value.map(item => Date(item.bidding_time))
+    // console.log(bidDate)
+    result.push({
+      label: key,
+      data: bids,
+      // date: bidDate,
+      borderColor: colors[index].hex,
+      fill: false,
+      pointBorderColor: "#fff",
+      pointRadius: 7,
+      pointBackgroundColor: colors[index].hex,
+    });
+  });
+
+  const chartEngData = {
+    labels: labels,
+    datasets: result
+  }
+
+  console.log('result sealed:', chartEngData);
+  return chartEngData;
 }
 
 
@@ -162,6 +201,11 @@ function App() {
         />
         <AuctionChart
           title='Sealed - Reverse'
+          bidHistory={computeDataSealed(sealed_forward)}
+          auctionType={2}
+        />
+        <AuctionChart
+          title='Sealed - Reverse'
           bidHistory={data_sealed_reverse(10)}
           auctionType={2}
         />
@@ -171,7 +215,7 @@ function App() {
           auctionType={1}
           options={legendOptions}
         />
-        
+
         {/* <Box sx={{ width: 900, height: 450 }}>
           <Line data={computeDataEnglish(english_revert)} options={legendOptions} />
         </Box> */}
