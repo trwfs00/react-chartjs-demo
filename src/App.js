023fc877ac1606d1aa05c77bd9f24a03283739b4
trwfs00 <teerawut.s@kkumail.com>
2024-03-07 13:27:42 +0700

    fix: Auction Chart

--- src/App.js
+++ src/App.js
@@ -13,13 +13,14 @@ import zoomPlugin from "chartjs-plugin-zoom"
 import { Container, Stack } from "@mui/material"
 import "chartjs-plugin-zoom"
 import { data_english, data_sealed_reverse } from "./utils/constraint/dataset"
-import { colors } from "./utils/constraint/colors"
+import { colors } from './utils/constraint/colors'
 import AuctionChart from "./components/SealedReverse/AuctionChart"
 
-const english_reverse = require("./utils/english_reverse.json")
-const english_forward = require("./utils/english_forward.json")
-const sealed_reverse = require("./utils/sealed_reverse.json")
-const sealed_forward = require("./utils/sealed_forward.json")
+
+const english_reverse = require('./utils/english_reverse.json')
+const english_forward = require('./utils/english_forward.json')
+const sealed_reverse = require('./utils/sealed_reverse.json')
+const sealed_forward = require('./utils/sealed_forward.json')
 
 ChartJS.register(
   CategoryScale,
@@ -34,49 +35,46 @@ ChartJS.register(
 
 const labels = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
 
-const computeDataEnglishReverse = data => {
-  let companies = {}
+const computeDataEnglishReverse = (data) => {
+
+  let companies = {};
 
   data.result.forEach(result => {
-    const companyName = result.detail.company_name
-    const biddingAmount = result.detail.bidding_amount
+    const companyName = result.detail.company_name;
+    const biddingAmount = result.detail.bidding_amount;
     const biddingTime = result.detail.bidding_time // Format bidding time to MM-DD-YYYY
 
     if (!companies[companyName]) {
       companies[companyName] = {
         biddingAmounts: Array(data.result.length).fill(null), // Create array with null values
-        biddingTimes: Array(data.result.length).fill(null),
+        biddingTimes: Array(data.result.length).fill(null)
       }
     }
 
-    const index = data.result.length - data.result.indexOf(result) - 1 // Calculate the index from the end
+    const index = data.result.length - data.result.indexOf(result) - 1; // Calculate the index from the end
     // companies[companyName][index] = biddingAmount; // Insert the bidding amount at the calculated index
-    companies[companyName].biddingAmounts[index] = biddingAmount // Insert the bidding amount at the calculated index
-    companies[companyName].biddingTimes[index] = biddingTime // Insert the bidding time at the calculated index
-  })
+    companies[companyName].biddingAmounts[index] = biddingAmount; // Insert the bidding amount at the calculated index
+    companies[companyName].biddingTimes[index] = biddingTime; // Insert the bidding time at the calculated index
+  });
 
-  const transformedData = Object.entries(companies).map(
-    ([companyName, { biddingAmounts, biddingTimes }], index) => {
-      biddingAmounts.unshift(null) // Add null at the beginning of the array
-      biddingTimes.unshift(null) // Add null at the beginning of the array
-      return {
-        label: companyName,
-        data: biddingAmounts,
-        date: biddingTimes,
-        borderColor: "#000",
-        fill: false,
-        pointBorderColor: "#fff",
-        pointRadius: 7,
-        pointBackgroundColor: colors[index].hex,
-      }
+  const transformedData = Object.entries(companies).map(([companyName, { biddingAmounts, biddingTimes }], index) => {
+    biddingAmounts.unshift(null); // Add null at the beginning of the array
+    biddingTimes.unshift(null); // Add null at the beginning of the array
+    return {
+      label: companyName,
+      data: biddingAmounts,
+      date: biddingTimes,
+      borderColor: "#000",
+      fill: false,
+      pointBorderColor: "#fff",
+      pointRadius: 7,
+      pointBackgroundColor: colors[index].hex,
     }
-  )
+  });
 
   // จัด data ให้เส้น
-  const sortedBiddingAmounts = data.result
-    .map(item => item.detail.bidding_amount)
-    .sort((a, b) => b - a)
-  sortedBiddingAmounts.unshift(null)
+  const sortedBiddingAmounts = data.result.map(item => item.detail.bidding_amount).sort((a, b) => b - a);
+  sortedBiddingAmounts.unshift(null);
 
   const chartEngData = {
     labels: labels,
@@ -89,57 +87,54 @@ const computeDataEnglishReverse = data => {
         pointStyle: false,
         pointRadius: 0,
         pointHoverRadius: 0,
-        pointHitRadius: 0,
+        pointHitRadius: 0
       },
-    ],
+    ]
   }
   console.log("english:", chartEngData)
   return chartEngData
 }
 
-const computeDataEnglishForward = data => {
-  let companies = {}
+const computeDataEnglishForward = (data) => {
+
+  let companies = {};
 
   data.result.forEach(result => {
-    const companyName = result.detail.company_name
-    const biddingAmount = result.detail.bidding_amount
+    const companyName = result.detail.company_name;
+    const biddingAmount = result.detail.bidding_amount;
     const biddingTime = result.detail.bidding_time // Format bidding time to MM-DD-YYYY
 
     if (!companies[companyName]) {
       companies[companyName] = {
         biddingAmounts: Array(data.result.length).fill(null), // Create array with null values
-        biddingTimes: Array(data.result.length).fill(null),
+        biddingTimes: Array(data.result.length).fill(null)
       }
     }
 
-    const index = data.result.length - data.result.indexOf(result) - 1 // Calculate the index from the end
+    const index = data.result.length - data.result.indexOf(result) - 1; // Calculate the index from the end
     // companies[companyName][index] = biddingAmount; // Insert the bidding amount at the calculated index
-    companies[companyName].biddingAmounts[index] = biddingAmount // Insert the bidding amount at the calculated index
-    companies[companyName].biddingTimes[index] = biddingTime // Insert the bidding time at the calculated index
-  })
+    companies[companyName].biddingAmounts[index] = biddingAmount; // Insert the bidding amount at the calculated index
+    companies[companyName].biddingTimes[index] = biddingTime; // Insert the bidding time at the calculated index
+  });
 
-  const transformedData = Object.entries(companies).map(
-    ([companyName, { biddingAmounts, biddingTimes }], index) => {
-      biddingAmounts.unshift(null) // Add null at the beginning of the array
-      biddingTimes.unshift(null) // Add null at the beginning of the array
-      return {
-        label: companyName,
-        data: biddingAmounts,
-        date: biddingTimes,
-        borderColor: "#000",
-        fill: false,
-        pointBorderColor: "#fff",
-        pointRadius: 7,
-        pointBackgroundColor: colors[index].hex,
-      }
+  const transformedData = Object.entries(companies).map(([companyName, { biddingAmounts, biddingTimes }], index) => {
+    biddingAmounts.unshift(null); // Add null at the beginning of the array
+    biddingTimes.unshift(null); // Add null at the beginning of the array
+    return {
+      label: companyName,
+      data: biddingAmounts,
+      date: biddingTimes,
+      borderColor: "#000",
+      fill: false,
+      pointBorderColor: "#fff",
+      pointRadius: 7,
+      pointBackgroundColor: colors[index].hex,
     }
-  )
+  });
 
   // จัด data ให้เส้น
-  const sortedBiddingAmounts = data.result
-    .map(item => item.detail.bidding_amount)
-    .sort((a, b) => a - b)
-  sortedBiddingAmounts.unshift(null)
+  const sortedBiddingAmounts = data.result.map(item => item.detail.bidding_amount).sort((a, b) => a - b);
+  sortedBiddingAmounts.unshift(null);
 
   const chartEngData = {
     labels: labels,
@@ -152,27 +147,27 @@ const computeDataEnglishForward = data => {
         pointStyle: false,
         pointRadius: 0,
         pointHoverRadius: 0,
-        pointHitRadius: 0,
+        pointHitRadius: 0
       },
-    ],
+    ]
   }
   console.log("english:", chartEngData)
   return chartEngData
 }
 
-const computeDataSealedReverse = data => {
-  const result = []
-  const companyData = {}
+const computeDataSealedReverse = (data) => {
+  const result = [];
+  const companyData = {};
 
-  ;[...data.result].reverse().forEach((item, index, arr) => {
-    const { company_name, bidding_amount, bidding_time } = item.detail
+  [...data.result].reverse().forEach((item, index, arr) => {
+    const { company_name, bidding_amount, bidding_time } = item.detail;
     if (!companyData[company_name]) {
-      companyData[company_name] = []
+      companyData[company_name] = [];
     }
-    companyData[company_name].push({ bidding_amount, bidding_time })
-  }, data.result.length - 1)
+    companyData[company_name].push({ bidding_amount, bidding_time });
+  }, data.result.length - 1);
 
-  console.log("companyData:", companyData)
+  console.log('companyData:', companyData)
 
   Object.entries(companyData).forEach(([key, value], index) => {
     let bids = value.map(item => item.bidding_amount)
@@ -188,31 +183,31 @@ const computeDataSealedReverse = data => {
       pointBorderColor: "#fff",
       pointRadius: 7,
       pointBackgroundColor: colors[index].hex,
-    })
-  })
+    });
+  });
 
   const chartEngData = {
     labels: labels,
-    datasets: result,
+    datasets: result
   }
 
-  console.log("result sealed:", chartEngData)
-  return chartEngData
+  console.log('result sealed:', chartEngData);
+  return chartEngData;
 }
 
-const computeDataSealedForward = data => {
-  const result = []
-  const companyData = {}
+const computeDataSealedForward = (data) => {
+  const result = [];
+  const companyData = {};
 
-  ;[...data.result].reverse().forEach((item, index, arr) => {
-    const { company_name, bidding_amount, bidding_time } = item.detail
+  [...data.result].reverse().forEach((item, index, arr) => {
+    const { company_name, bidding_amount, bidding_time } = item.detail;
     if (!companyData[company_name]) {
-      companyData[company_name] = []
+      companyData[company_name] = [];
     }
-    companyData[company_name].push({ bidding_amount, bidding_time })
-  }, data.result.length - 1)
+    companyData[company_name].push({ bidding_amount, bidding_time });
+  }, data.result.length - 1);
 
-  console.log("companyData:", companyData)
+  console.log('companyData:', companyData)
 
   Object.entries(companyData).forEach(([key, value], index) => {
     let bids = value.map(item => item.bidding_amount)
@@ -228,17 +223,18 @@ const computeDataSealedForward = data => {
       pointBorderColor: "#fff",
       pointRadius: 7,
       pointBackgroundColor: colors[index].hex,
-    })
-  })
+    });
+  });
 
   const chartEngData = {
     labels: labels,
-    datasets: result,
+    datasets: result
   }
 
-  console.log("result sealed:", chartEngData)
-  return chartEngData
+  console.log('result sealed:', chartEngData);
+  return chartEngData;
 }
+
 
 function App() {
   return (
@@ -255,16 +251,16 @@ function App() {
           auctionType={1}
         />
         <AuctionChart
-          title='Sealed - Forward'
-          bidHistory={computeDataSealedForward(sealed_forward)}
-          auctionType={2}
-          reservedPrice={1800000}
-        />
-        <AuctionChart
           title='Sealed - Reverse'
           bidHistory={computeDataSealedReverse(sealed_reverse)}
           auctionType={2}
           reservedPrice={1000000}
+        />
+        <AuctionChart
+          title='Sealed - Forward'
+          bidHistory={computeDataSealedForward(sealed_forward)}
+          auctionType={2}
+          reservedPrice={1800000}
         />
       </Stack>
     </Container>
